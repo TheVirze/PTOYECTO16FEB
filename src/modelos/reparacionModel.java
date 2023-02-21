f457f1508a903d0ae65fe2351eb6b81a3628a3e4
Jordy Aguilar <Jordy Aguilar@TufGaming>
2023-02-21 00:44:48 -0600

    Jordy

--- src/modelos/reparacionModel.java
+++ src/modelos/reparacionModel.java
@@ -1,5 +1,16 @@
 package modelos;
 
+import java.util.ArrayList;
+
 public class reparacionModel {
+    public ArrayList<reparacion> ListaReparacion = new ArrayList<reparacion>();
     
+     public void AgregarReparacion (String codigo, String reparacion, String tecnico, String fecha){
+         reparacion nuevaReparacion = new reparacion (codigo, reparacion, tecnico, fecha);
+         this.ListaReparacion.add(nuevaReparacion);
+     }
+     
+     public ArrayList ListaReparacion() {
+        return ListaReparacion;
+    }    
 }
