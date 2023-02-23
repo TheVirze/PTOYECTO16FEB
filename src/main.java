45734533cc0d3d8ed9bbc1ff592d2b005e6108aa
Jordy Aguilar <Jordy Aguilar@192.168.209.29>
2023-02-23 12:46:47 -0600

    Jordy

--- src/main.java
+++ src/main.java
@@ -1,6 +1,27 @@
+
+import vistas.frmPrincipal;
+import vistas.frmIngreso;
+import vistas.frmPropietarios;
+import vistas.frmReparacion;
+import modelos.entregaModel;
+import modelos.ingresosModel;
+import modelos.propietariosModel;
+import modelos.reparacionModel;
+import controladores.EntregaController;
+import controladores.IngresoController;
+import controladores.PropietariosController;
+import controladores.reparacionController;
+
 public class main {
 
     public static void main(String[] args) {
+     
+        frmPrincipal VistaPrincipal = new frmPrincipal();
+        frmIngreso VistaIngreso = new frmIngreso (VistaPrincipal, true);
+        usuariosModel ModeloUsuarios = new usuariosModel();
+        
+        usuariosController ControladorUsuarios = new usuariosController(VistaPrincipal,
+                VistaUsuarios, ModeloUsuarios);        
         
     }
     
